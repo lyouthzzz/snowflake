c9c5ac0532b008c781a5c218650f70e44e821dd7
yang.liu14 <yang.liu14@weimob.com>
2021-04-26 18:30:01 +0800

    snowflake id generator

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lyouthzzz/snowflake
+
+go 1.16
+
+require github.com/stretchr/testify v1.7.0
---
+++ snowflake_test.go
@@ -0,0 +1,41 @@
+package snowflake
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSnowFlake(t *testing.T) {
+	node := NewNode()
+
+	var (
+		wg     sync.WaitGroup
+		worker = 1000
+		idMap  = make(map[int64]bool)
+		mu     sync.RWMutex
+	)
+
+	for i := 0; i <= worker; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for ii := 0; ii <= 100; ii++ {
+				id, err := node.NextId()
+				require.NoError(t, err)
+				fmt.Println(id)
+
+				mu.Lock()
+				if _, exist := idMap[id]; exist {
+					t.Error(errors.New("duplicate id"))
+				}
+				mu.Unlock()
+			}
+		}()
+	}
+
+	wg.Wait()
+}
